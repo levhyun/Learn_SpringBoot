9a475fcf41d5050136232c9d5ccf97a8200fd90f
lev._.hyun <110030247+levhyun@users.noreply.github.com>
2023-01-30 14:03:58 +0900

    .

--- src/main/java/com/example/springproject/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/springproject/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.example.springproject.repository;
 
 import com.example.springproject.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
--- src/main/java/com/example/springproject/service/MemberService.java
+++ src/main/java/com/example/springproject/service/MemberService.java
@@ -3,13 +3,17 @@ package com.example.springproject.service;
 import com.example.springproject.domain.Member;
 import com.example.springproject.repository.MemberRepository;
 import com.example.springproject.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
